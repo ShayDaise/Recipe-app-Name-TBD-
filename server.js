dba57f1d5dd3e026a2e56199d1a0c78ce3ea1965
rrvallar <rrvallar@gmail.com>
2022-08-16 20:23:12 -0400

    develop push
    push

--- server.js
+++ server.js
@@ -40,6 +40,3 @@ sequelize.sync({ force: true }).then(() => {
   seedAll();
   app.listen(PORT, () => console.log("Now listening"));
 });
-
-// sequalize.sync run first before seeds otherwise tables wont exist***
-// changed .sync from fale to true to drop tables
